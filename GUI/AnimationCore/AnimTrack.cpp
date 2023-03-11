5a6c2d62bedd95420dcdd97118aaf0d5529d3e65
Preey Zhu <mysteryworldgod@outlook.com>
2023-03-11 11:15:09 +0800

    Update License Notice

--- GUI/AnimationCore/AnimTrack.cpp
+++ GUI/AnimationCore/AnimTrack.cpp
@@ -1,25 +1,25 @@
 /**
-* @file    AnimFrame.cpp
+* @file    AnimTrack.cpp
 * @author  Tyler Parret Zhu (OwlHowlinMornSky) <mysteryworldgod@outlook.com>
 *
 * @section LICENSE
 *
-*				GNU AFFERO GENERAL PUBLIC LICENSE
-*				   Version 3, 19 November 2007
+*                   GNU AFFERO GENERAL PUBLIC LICENSE
+*                      Version 3, 19 November 2007
 *
-* Copyright (c) 2023  Tyler Parret Zhu
-*
+*    Copyright (c) 2023  Tyler Parret Zhu
+* 
 *    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
+*    it under the terms of the GNU Affero General Public License as published
+*    by the Free Software Foundation, either version 3 of the License, or
 *    (at your option) any later version.
 *
 *    This program is distributed in the hope that it will be useful,
 *    but WITHOUT ANY WARRANTY; without even the implied warranty of
 *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
+*    GNU Affero General Public License for more details.
 *
-*    You should have received a copy of the GNU General Public License
+*    You should have received a copy of the GNU Affero General Public License
 *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
 */
